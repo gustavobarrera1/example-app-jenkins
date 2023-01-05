e80b37597f374769138404ae739d934a0d387e81
Gustavo Barrera <gbarrera@cstechlab.com>
2023-01-04 21:23:14 -0300

    Arreglando errores

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
+import time
+
+import redis
 from flask import Flask
-from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'gustavo'
-app.config['MYSQL_HOST'] = 'http://34.176.51.200/'
-app.config['MYSQL_DB'] = 'mysql'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-mysql = MySQL(app)
-
-# mysql.init_app(app)
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
-def CONNECT_DB():
-    CS = mysql.connection.cursor()
-    CS.execute('''CREATE TABLE Empleados (id INTEGER, name VARCHAR(20))''')
-
-    CS.execute('''INSERT INTO Empleados VALUES (1, 'Gustavo')''')
-    CS.execute('''INSERT INTO Empleados VALUES (2, 'Victor')''')
-    mysql.connection.commit()
-    return 'Executed successfully'
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def hello():
+    count = get_hit_count()
+    return 'Hola mundo!!! Has recargado {} veces.\n'.format(count)
